edeebe28222dc533e49b2f136579e60e6e2b0498
VajkFreyasman <craig.linde.com>
2018-05-10 12:00:17 -0600

    added reactstrap and further stylings

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,9 +1,65 @@
 import React from 'react';
-
+import {
+  Col,
+  Row 
+} from 'reactstrap'
 
 class MadlibForm extends React.Component {
+  
+  handleChange = function(){
+    console.log('Trying to handle change');
+  }
+
   render() {
-    return <h1>Bottega MadlibsForm Component</h1>;
+    return (
+      <Row style ={{textAlign: "center", color: "white"}}>
+        <Col md="3"  className = "inputWrapper">
+          <Row>
+            <Col md = "2">
+              <label className = "greenLabel">1</label>
+            </Col>
+            <Col md = "10">
+              <input placeholder = "Color" type = "text" onChange={this.handleChange}/>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col md="3"  className = "inputWrapper">
+          <Row>
+            <Col md = "2">
+              <label className = "greenLabel">2</label>
+            </Col>
+            <Col md = "10">
+              <input placeholder = "Color" type = "text" onChange={this.handleChange} />
+            </Col>
+          </Row>
+        </Col>
+
+        <Col md="3"  className = "inputWrapper">
+          <Row>
+            <Col md = "2">
+              <label className = "greenLabel">3</label>
+            </Col>
+            <Col md = "10">
+              <input placeholder = "Color" type = "text" onChange={this.handleChange} />
+            </Col>
+          </Row>
+        </Col>
+
+        <Col md="3"  className = "inputWrapper">
+          <Row>
+            <Col md = "2">
+              <label className = "greenLabel">4</label>
+            </Col>
+            <Col md = "10">
+              <input placeholder = "Color" type = "text" onChange={this.handleChange} />
+            </Col>
+          </Row>
+        </Col>
+
+
+      </Row>
+      );
   }
 }
 
