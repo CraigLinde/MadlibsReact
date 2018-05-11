ca2dae9a3ab399c491723f12e3bde151115b729d
VajkFreyasman <craig.linde.com>
2018-05-11 11:59:25 -0600

    added scss partials and modified stylings

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
   Col,
-  Row 
+  Row,
+  Card 
 } from 'reactstrap'
 
 class MadlibForm extends React.Component {
@@ -27,73 +28,83 @@ class MadlibForm extends React.Component {
 
   render() {
     return (
-      <Row style ={{textAlign: "center", color: "white"}}>
-        <Col md="3"  className = "inputWrapper">
+      <div className = "card-wrapper">
+        <Card>
+          <Row style ={{textAlign: "center", color: "white"}}>
+        <Col md="3"  className = "input-wrapper">
           <Row>
             <Col md = "2">
-              <label className = "greenLabel">1</label>
+              <label className = "green-label">1</label>
             </Col>
             <Col md = "10">
               <input placeholder = "Color" type = "text" onChange={this.handleChange({inputTitle: 'color'})}/>
             </Col>
           </Row>
           <Row>
-            <Col md="12">
-              <div className = "inputDescription">Color</div>
+            <Col md='2'></Col>
+            <Col md="10">
+              <div className = "input-description">Color</div>
             </Col>
           </Row>
         </Col>
 
-        <Col md="3"  className = "inputWrapper">
+        <Col md="3"  className = "input-wrapper">
           <Row>
             <Col md = "2">
-              <label className = "greenLabel">2</label>
+              <label className = "green-label">2</label>
             </Col>
             <Col md = "10">
               <input placeholder = "Noun (Plural)" type = "text" onChange={this.handleChange({inputTitle: 'pluralNoun'})}/>
             </Col>
           </Row>
           <Row>
-            <Col md="12">
-              <div className = "inputDescription">Noun (Plural)</div>
+            <Col md='2'></Col>
+            <Col md="10">
+              <div className = "input-description">Noun (Plural)</div>
             </Col>
           </Row>
         </Col>
 
-        <Col md="3"  className = "inputWrapper">
+        <Col md="3"  className = "input-wrapper">
           <Row>
             <Col md = "2">
-              <label className = "greenLabel">3</label>
+              <label className = "green-label">3</label>
             </Col>
             <Col md = "10">
               <input placeholder = "Adjective" type = "text" onChange={this.handleChange({inputTitle: 'adjectiveOne'})}/>
             </Col>
           </Row>
           <Row>
-            <Col md="12">
-              <div className = "inputDescription">Adjective(Plural)</div>
+            <Col md='2'></Col>
+            <Col md="10">
+              <div className = "input-description">Adjective(Plural)</div>
             </Col>
           </Row>
         </Col>
 
-        <Col md="3"  className = "inputWrapper">
+        <Col md="3"  className = "input-wrapper">
           <Row>
             <Col md = "2">
-              <label className = "greenLabel">4</label>
+              <label className = "green-label">4</label>
             </Col>
             <Col md = "10">
               <input placeholder = "Celebrity" type = "text" onChange={this.handleChange({inputTitle: 'celebrityOne'})}/>
             </Col>
           </Row>
           <Row>
-            <Col md="12">
-              <div className = "inputDescription">Celebrity</div>
+            <Col md='2'></Col>
+            <Col md="10">
+              <div className = "input-description">Celebrity</div>
             </Col>
           </Row>
         </Col>
 
 
       </Row>
+
+        </Card>
+      </div>
+      
       );
   }
 }
